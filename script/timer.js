9d0ea31d3d11b94d80dd62c1f54aee5516eeac7b
zhaoqi <911379608@qq.com>
2016-03-28 14:53:40 +0800

    20160328 14:53提交后点击reset的问题没有解决

--- script/timer.js
+++ script/timer.js
@@ -4,8 +4,54 @@ $('document').ready(function() {
 		msec = 0;
 	var timerInterval;
 	var clicknumber_pause = 0,
-		clciknumber_count = 0;
+		clicknumber_count = 0;
 	var text = '';
+	var listNumber = 0;
+	var studentList = new Array();
+	$('#Ok').attr('disabled', 'disabled');
+
+	function Student(name, order, grade) {
+		this.name = name;
+		this.order = order;
+		this.grade = grade;
+	}
+
+	function getHistory() {
+		var history = localStorage.getItem('history');
+		if (!history) {
+			history = '';
+		}
+		$('#history').empty().show();
+		$('#result').empty();
+		var historyItem = history.split(',');
+		for (var j = 1; j < historyItem.length; j++) {
+			var newTitle = '<li class="list-group-item historyItem btn">' + historyItem[j] + '</li>';
+			$('#history').append(newTitle);
+		}
+		$('li.historyItem').click(function() {
+			$('#history').hide();
+			var tableName = $(this).text();
+			var table = localStorage.getItem(tableName);
+			table = JSON.parse(table);
+			for (var i = 0; i < table.length; i++) {
+				var name = table[i].name;
+				var order = table[i].order;
+				var grade = table[i].grade;
+				var item = "<tr><td>No." + order + '</td><td>' + grade + '</td><td>' + name + '</td></tr>';
+				$('#result').append(item);
+			}
+			// var deleteButton = '<button class="btn deleteTable">delete</button>';
+			// $('#result').append(deleteButton);
+			// localStorage.removeItem(tableName);
+			// $('button .deleteTable').click(function() {
+			// 	var p = history.indexOf(tableName);
+			// 	history.splice(p-1, tableName.length+1);
+			// 	alert(history);
+			// 	localStorage.setItem(history);
+			// 	alert('delete'+tableName);
+			// });
+		});
+	}
 
 	function addZeroToTime() {
 		if (msec < 10) {
@@ -42,15 +88,20 @@ $('document').ready(function() {
 		msec = 0;
 		sec = 0;
 		min = 0;
-		clciknumber_count = 0;
+		clicknumber_count = 0;
+		listNumber = 0;
 		$('h1').text('00:00:00');
 		$('#start').text('start');
 		$('#result').empty();
-		$('#grade').empty();
+		$('#grade').empty().show();
+		$('#history').empty();
 		$('#input_name div').remove();
-		$('#show').removeClass('disabled');
-		$('#start').removeClass('disabled');
-		$('#count').removeClass('disabled');
+		$('#show').removeAttr('disabled');
+		$('#start').removeAttr('disabled');
+		$('#count').removeAttr('disabled');
+		$('#toNext').removeAttr('disabled');
+		$('#Ok').removeAttr('disabled');
+		studentList = new Array();
 	});
 	$('#start').click(function() {
 		if (clicknumber_pause % 2) {
@@ -66,43 +117,64 @@ $('document').ready(function() {
 		clicknumber_pause++;
 	});
 	$('#count').click(function() {
-		clciknumber_count++;
+		clicknumber_count++;
 		addZeroToTime();
 		$('h1').text(text);
-
-		// var grade_table = '<tr><td>' + clciknumber_count + '</td><td>' + text + '</td></tr>';
-		var grade_div = '<tr><td class="order">No.' + clciknumber_count + '</td> <td class="time">' + text + '</td></tr>';
+		// var grade_table = '<tr><td>' + clicknumber_count + '</td><td>' + text + '</td></tr>';
+		var grade_div = '<tr><td class="order">No.' + clicknumber_count + '</td> <td class="time">' + text + '</td></tr>';
 		// $('#result').append(grade_table);
 		$('#grade').append(grade_div);
 	});
 	$('#show').click(function() {
 		$('#grade').hide();
-		$('#show').addClass('disabled');
-		$('#count').addClass('disabled');
-		$('#start').addClass('disabled');
-		var i = 0;
+		$('#show').attr('disabled', 'disabled');
+		$('#count').attr('disabled', 'disabled');
+		$('#start').attr('disabled', 'disabled');
+		clearInterval(timerInterval);
 		var time = $('#grade tr .time');
 		var order = $('#grade tr .order');
-		var nameForStudent = '<div>' + time.eq(i).text() + '</div>';
-		$('#input_name').append(nameForStudent);
+		var gradeForStudent = '<div>' + time.eq(listNumber).text() + '</div>';
+		$('#input_name').append(gradeForStudent);
 		// var item = "<tr><td>"+i+'</td><td>'+time.eq(i).text()+'</td><td>'+name+'</td></tr>';
 		// $('#result').append(item);
 		$('#toNext').click(function(event) {
 			/* Act on the event */
-			if (i < time.length) {
-				var name = $('#input_name input').val();
+			if (listNumber < time.length) {
+				var name = $('#input_name input#addName').val();
 				if (name) {
-					var item = "<tr><td>No." + (i + 1) + '</td><td>' + time.eq(i).text() + '</td><td>' + name + '</td></tr>';
+					var item = "<tr><td>No." + (listNumber + 1) + '</td><td>' + time.eq(listNumber).text() + '</td><td>' + name + '</td></tr>';
 					$('#result').append(item);
-					i++;
-					nameForStudent = '<div>' + time.eq(i).text() + '</div>';
-					$('#input_name div').replaceWith(nameForStudent);
-					$('#input_name input').val('');
+					var newStudent = new Student(name, listNumber + 1, time.eq(listNumber).text());
+					studentList.push(newStudent);
+					listNumber++;
+					gradeForStudent = '<div>' + time.eq(listNumber).text() + '</div>';
+					$('#input_name div').replaceWith(gradeForStudent);
+					$('#input_name input#addName').val('');
 				}
 			}
+			if (listNumber == time.length) {
+				$('#toNext').attr('disabled', 'disabled');
+				$('#Ok').removeAttr('disabled');
+			}
+		});
+		$('#Ok').click(function() {
+			var history = localStorage.getItem('history');
+			if (!history) {
+				history = '';
+			}
+			$('#input_name input#addName').val('');
+			$('#Ok').attr('disabled', 'disabled');
+			var newTittle = $('#input_name input#addTittle').val();
+			history += ',' + newTittle;
+			localStorage.setItem('history', history);
+			var storageStydentList = JSON.stringify(studentList);
+			localStorage.setItem(newTittle, storageStydentList);
+			if (newTittle) {
+				$('#history').append('<li class="list-group-item">' + newTittle + '</li>');
+			}
+			$('#input_name input#addTittle').val('');
+			listNumber = 0;
 		});
 	});
-	$('#Ok').click(function() {
-		$('#show').removeClass('disabled');
-	});
+	$('#showHistory').click(getHistory);
 });
